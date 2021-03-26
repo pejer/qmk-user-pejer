bd026996c5fa98f62586b2152c66bede58aa0793
Henrik Pejer <mrpejer@gmail.com>
2021-03-26 14:01:49 +0100

    cleanup: Nav layer tweaks

--- wrappers.h
+++ wrappers.h
@@ -199,9 +199,9 @@ Shamefully stolen from Drashna.
 #define ___________PEJER_SYMBOL1_L2________________        KC_A, KC_S, KC_D, KC_F,     KC_G
 #define ___________PEJER_SYMBOL1_L3________________         KC_Z,     KC_X,     KC_C,     KC_V,     KC_B
 
-#define ___________PEJER_SYMBOL1_R1________________         KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_AGAIN
+#define ___________PEJER_SYMBOL1_R1________________         KC_AGAIN, KC_PSTE,  KC_COPY, KC_CUT, KC_UNDO
 #define ___________PEJER_SYMBOL1_R2________________         KC_LEFT, KC_DOWN,    KC_UP, KC_RIGHT, KC_CAPS
-#define ___________PEJER_SYMBOL1_R3________________         KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_XXXXX
+#define ___________PEJER_SYMBOL1_R3________________         KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_DEL
 
  
 #define ___________PEJER_NUMBER_L1_________________        KC_1,     KC_2,     KC_3,     KC_4,     KC_5 

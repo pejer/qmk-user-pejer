010750bb9b734a5449fc012db607028818fe21df
Henrik Pejer <mrpejer@gmail.com>
2021-05-12 14:39:53 +0200

    cleanup: Reverting debounce

--- rules.mk
+++ rules.mk
@@ -7,7 +7,7 @@ EXTRAKEY_ENABLE 			= yes
 
 # Debounce eagerly (report change immediately), keep per-key timers. We can use
 # this because the kinT does not have to deal with noise.
-DEBOUNCE_TYPE = sym_eager_pk
+# DEBOUNCE_TYPE = sym_eager_pk
 
 ifneq ($(strip $(NO_SECRETS)), yes)
     ifneq ("$(wildcard $(USER_PATH)/secrets.c)","")
